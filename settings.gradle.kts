c75d0cd5e54363040a6f859402c1a5b8c5725223
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2024-03-10 12:51:18 +0700

    initial kotlin setup

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "IF3210-2024-Android-PPL"
+include(":app")
+ 
