2068b78ca9099a06ec13f092b99c806150a00756
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2024-03-12 16:04:55 +0700

    feat: add login xml setup and fix some bug

---
+++ app/src/main/java/com/example/if3210_2024_android_ppl/ui/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package com.example.if3210_2024_android_ppl.ui.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
