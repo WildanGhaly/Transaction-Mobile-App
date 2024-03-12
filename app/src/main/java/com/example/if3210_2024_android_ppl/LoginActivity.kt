9cfc5f8ceb320df428cb99efdd5e15595bdad3a4
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2024-03-12 17:54:16 +0700

    feat: add login page with simple post request

---
+++ app/src/main/java/com/example/if3210_2024_android_ppl/LoginActivity.kt
@@ -0,0 +1,60 @@
+package com.example.if3210_2024_android_ppl
+import android.util.Log
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.if3210_2024_android_ppl.api.LoginRequest
+import com.example.if3210_2024_android_ppl.api.LoginResponse
+import com.example.if3210_2024_android_ppl.api.RetrofitInstance
+import com.example.if3210_2024_android_ppl.databinding.ActivityLoginBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding // Binding untuk layout XML login
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonLogin.setOnClickListener {
+            val email = binding.editTextEmail.text.toString()
+            val password = binding.editTextPassword.text.toString()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Log.d("LoginActivity", "Email or password is empty")
+            } else {
+                Log.d("LoginActivity", "Email: $email, Password: $password")
+
+                RetrofitInstance.api.login(LoginRequest(email, password))
+                    .enqueue(object : Callback<LoginResponse> {
+                        override fun onResponse(
+                            call: Call<LoginResponse>,
+                            response: Response<LoginResponse>
+                        ) {
+                            if (response.isSuccessful) {
+                                val loginResponse = response.body()
+                                // Handle successful login response
+                                if (loginResponse != null) {
+                                    Log.d("LoginActivity", "Login successful, Token: ${loginResponse.token}")
+                                    val intent = Intent(this@LoginActivity, MainActivity::class.java)
+                                    startActivity(intent)
+                                    finish()
+                                }
+                            } else {
+                                // Handle unsuccessful login response
+                                Log.d("LoginActivity", "Login failed")
+                            }
+                        }
+
+                        override fun onFailure(call: Call<LoginResponse>, t: Throwable) {
+                            // Handle failure
+                            Log.e("LoginActivity", "Login failed", t)
+                        }
+                    })
+            }
+        }
+    }
+}
