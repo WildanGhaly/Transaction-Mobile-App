853830a583e79f7474c59bee2f8aa72c5866958e
Raditss <raditya.naufal.a@gmail.com>
2024-03-14 15:53:02 +0700

    cam preview(untested)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("androidx.compose.runtime:runtime-android:1.6.3")
 
     val cameraxVersion = "1.3.2"
     implementation("androidx.camera:camera-core:$cameraxVersion")
@@ -60,7 +61,7 @@ dependencies {
     implementation("androidx.camera:camera-lifecycle:$cameraxVersion")
     implementation("androidx.camera:camera-view:$cameraxVersion")
     implementation("androidx.camera:camera-extensions:$cameraxVersion")
-    implementation("androidx.annotation:annotation:1.6.0")
+    implementation("androidx.annotation:annotation:1.7.1")
     
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
--- app/src/main/java/com/example/if3210_2024_android_ppl/ui/scan/ScanFragment.kt
+++ app/src/main/java/com/example/if3210_2024_android_ppl/ui/scan/ScanFragment.kt
@@ -1,5 +1,6 @@
 package com.example.if3210_2024_android_ppl.ui.scan
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,19 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.if3210_2024_android_ppl.databinding.FragmentScanBinding
+import android.content.DialogInterface
+import android.content.pm.PackageManager
+import android.graphics.Camera
+import android.widget.Button
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.LifecycleCameraController
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.common.util.concurrent.ListenableFuture
 
 class ScanFragment : Fragment() {
 
@@ -16,19 +30,75 @@ class ScanFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+    private lateinit var cameraController: LifecycleCameraController
+
+
+    private val cameraProviderFuture: ListenableFuture<ProcessCameraProvider> by lazy {
+        ProcessCameraProvider.getInstance(requireContext())
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val scanViewModel =
-            ViewModelProvider(this).get(ScanViewModel::class.java)
-
         _binding = FragmentScanBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        if (!hasRequiredPermission()) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                CAMERAX_PERMISSION,
+                0
+            )
+        }
+
+//        create onclick listener for button
+        binding.shutterBut.setOnClickListener {
+
+        }
+        binding.flashBut.setOnClickListener {
+            //create a toast
+            Toast.makeText(context, "Flash Clicked", Toast.LENGTH_SHORT).show()
+
+        }
+        binding.pictBut.setOnClickListener {
+            //create a toast
+            Toast.makeText(context, "Picture Clicked", Toast.LENGTH_SHORT).show()
+
+            }
+        initCamera()
+
         return root
+    }
+
+    private fun initCamera() {
+        cameraProviderFuture.addListener({
+            val cameraProvider = cameraProviderFuture.get()
+            cameraController = LifecycleCameraController(requireContext())
+            cameraController.bindToLifecycle(viewLifecycleOwner)
+
+            val preview = binding.prev // Access the existing PreviewView
+            preview.controller = cameraController
+
+        }, ContextCompat.getMainExecutor(requireContext()))
+    }
+
+    private fun hasRequiredPermission(): Boolean {
+        return CAMERAX_PERMISSION.all {
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                it
+            ) == PackageManager.PERMISSION_GRANTED
+        }
+    }
+
+    companion object {
+        private val CAMERAX_PERMISSION = arrayOf(
+            android.Manifest.permission.CAMERA,
+            android.Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
     }
 
     override fun onDestroyView() {
