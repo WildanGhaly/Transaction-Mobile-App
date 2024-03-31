88c6a42358a5eed30200c4d7bf56c4cf8ec74c08
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2024-04-01 06:36:09 +0700

    feat: add save file and share mechanism (dummy)

--- app/src/main/java/com/example/if3210_2024_android_ppl/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/example/if3210_2024_android_ppl/ui/setting/SettingFragment.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.if3210_2024_android_ppl.database.transaction.Transaction
 import com.example.if3210_2024_android_ppl.database.user.UserViewModel
@@ -37,6 +38,7 @@ class SettingFragment : Fragment() {
         val root: View = binding.root
 
         userViewModel = ViewModelProvider(this).get(UserViewModel::class.java)
+
         setupButtonListeners()
 
         return root
@@ -60,13 +62,35 @@ class SettingFragment : Fragment() {
     private fun setupButtonListeners() {
         binding.buttonSendTransaction.setOnClickListener {
             // TODO: Handle send transaction button click
-            val transaction = listOf<Transaction>()
-            sendReport(transaction)
+            userViewModel.getActiveUserEmail { email ->
+                if (email != null) {
+                    Log.d("SettingFragment", "Active user's email: $email")
+
+                    // TODO: getTransactions()
+                    val transaction = listOf<Transaction>()
+                    sendReport(transaction)
+
+                } else {
+                    Log.e("SettingFragment", "Failed to retrieve active user's email")
+                }
+            }
+
             Log.d("SettingFragment", "on click")
         }
 
         binding.buttonSaveTransaction.setOnClickListener {
             // TODO: Handle save transaction button click
+            val transactions = listOf<Transaction>()
+            context?.let { ctx ->
+                val excelFileCreator = ExcelFileCreator(ctx)
+                val fileUri = excelFileCreator.createExcelFile(transactions)
+                val shareIntent = Intent(Intent.ACTION_SEND).apply {
+                    type = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                    putExtra(Intent.EXTRA_STREAM, fileUri)
+                    addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                }
+                startActivity(Intent.createChooser(shareIntent, "Share Excel File"))
+            }
         }
 
         binding.buttonBelow.setOnClickListener {
