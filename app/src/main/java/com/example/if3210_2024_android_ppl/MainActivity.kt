fee0a97c3737e1426ccb1c9ae7d885de89d63f01
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2024-04-01 08:10:52 +0700

    feat: add dummy setup token request

--- app/src/main/java/com/example/if3210_2024_android_ppl/MainActivity.kt
+++ app/src/main/java/com/example/if3210_2024_android_ppl/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.if3210_2024_android_ppl
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
@@ -8,9 +9,15 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.if3210_2024_android_ppl.api.KeystoreHelper
+import com.example.if3210_2024_android_ppl.api.RetrofitInstance
+import com.example.if3210_2024_android_ppl.api.TokenResponse
 import com.example.if3210_2024_android_ppl.database.user.User
 import com.example.if3210_2024_android_ppl.database.user.UserViewModel
 import com.example.if3210_2024_android_ppl.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,6 +30,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
+
+        val keystoreHelper = KeystoreHelper(this@MainActivity)
+        val token = keystoreHelper.getToken()
+        val authorizationHeaderValue = "Bearer $token"
+        val call: Call<TokenResponse> = RetrofitInstance.api.token(authorizationHeaderValue)
+        call.enqueue(object : Callback<TokenResponse> {
+            override fun onResponse(call: Call<TokenResponse>, response: Response<TokenResponse>) {
+                if (response.isSuccessful) {
+                    val tokenResponse = response.body()
+                    if (tokenResponse != null) {
+                        Log.d("Main", tokenResponse.nim)
+                    }
+                } else {
+                    // Handle request errors
+                }
+            }
+
+            override fun onFailure(call: Call<TokenResponse>, t: Throwable) {
+                // Handle failure, such as a network error
+            }
+        })
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
--- app/src/main/java/com/example/if3210_2024_android_ppl/api/ApiService.kt
+++ app/src/main/java/com/example/if3210_2024_android_ppl/api/ApiService.kt
@@ -1,12 +1,17 @@
 import com.example.if3210_2024_android_ppl.api.LoginRequest
 import com.example.if3210_2024_android_ppl.api.LoginResponse
+import com.example.if3210_2024_android_ppl.api.TokenResponse
 import retrofit2.Call
 
 import retrofit2.http.Body
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface ApiService {
 
     @POST("api/auth/login")
     fun login(@Body request: LoginRequest): Call<LoginResponse>
+
+    @POST("api/auth/token")
+    fun token(@Header("Authorization") authorization: String): Call<TokenResponse>
 }
