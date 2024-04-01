148363f4c3f91f2f042c85fa5f5931b75f912be1
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2024-04-01 17:45:35 +0700

    feat: add loading for buttons in setting fragment

--- app/src/main/java/com/example/if3210_2024_android_ppl/ui/setting/SettingFragment.kt
+++ app/src/main/java/com/example/if3210_2024_android_ppl/ui/setting/SettingFragment.kt
@@ -18,18 +18,24 @@ import com.example.if3210_2024_android_ppl.database.transaction.Transaction
 import com.example.if3210_2024_android_ppl.database.transaction.TransactionDatabase
 import com.example.if3210_2024_android_ppl.database.user.UserViewModel
 import com.example.if3210_2024_android_ppl.databinding.FragmentSettingBinding
+import com.example.if3210_2024_android_ppl.util.DialogUtils
 import com.example.if3210_2024_android_ppl.util.EmailSender
 import com.example.if3210_2024_android_ppl.util.ExcelFileCreator
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import kotlinx.coroutines.TimeoutCancellationException
+import kotlinx.coroutines.withTimeoutOrNull
 
 class SettingFragment : Fragment() {
 
     private var _binding: FragmentSettingBinding? = null
     private lateinit var userViewModel: UserViewModel
     private val db by lazy { TransactionDatabase(requireContext()) }
+    private var isOperationInProgress = false
+    private var isSaveOperationInProgress = false
+    private var isLogoutOperationInProgress = false
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -77,65 +83,114 @@ class SettingFragment : Fragment() {
         }
     }
 
-
     private fun setupButtonListeners() {
         binding.buttonSendTransaction.setOnClickListener {
-            // TODO: Handle send transaction button click
-            userViewModel.getActiveUserEmail { email ->
-                if (email != null) {
-                    Log.d("SettingFragment", "Active user's email: $email")
-
-                    // TODO: getTransactions()
-                    CoroutineScope(Dispatchers.IO).launch {
-                        val transactions = db.transactionDao().getTransactions()
-                        val isXlsxFormat = binding.switchXlsXlsx.isChecked // xlsx true, else false
-                        sendReport(transactions, isXlsxFormat)
-                        Log.d("IsiTransaksi", transactions.toString())
+            if (!isOperationInProgress) {
+                isOperationInProgress = true
+                val dialog = DialogUtils.showLoadingDialog(requireContext())
+                userViewModel.getActiveUserEmail { email ->
+                    if (email != null) {
+                        lifecycleScope.launch {
+                            try {
+                                val job = withTimeoutOrNull(10000) {
+                                    CoroutineScope(Dispatchers.IO).launch {
+                                        val transactions = db.transactionDao().getTransactions()
+                                        val isXlsxFormat = binding.switchXlsXlsx.isChecked
+                                        sendReport(transactions, isXlsxFormat)
+                                    }
+                                }
+                                if (job == null) {
+                                    DialogUtils.showTimeoutDialog(requireContext())
+                                }
+                            } catch (e: TimeoutCancellationException) {
+                                DialogUtils.showTimeoutDialog(requireContext())
+                            } finally {
+                                dialog.dismiss()
+                                isOperationInProgress = false
+                            }
+                        }
+                    } else {
+                        Log.e("SettingFragment", "Failed to retrieve active user's email")
+                        dialog.dismiss()
+                        isOperationInProgress = false
                     }
-                } else {
-                    Log.e("SettingFragment", "Failed to retrieve active user's email")
                 }
             }
-
-            Log.d("SettingFragment", "on click")
         }
 
         binding.buttonSaveTransaction.setOnClickListener {
-            // TODO: Handle save transaction button click
-            CoroutineScope(Dispatchers.IO).launch {
-                val transactions = db.transactionDao().getTransactions()
-                val isXlsxFormat = binding.switchXlsXlsx.isChecked // xlsx true, else false
-                context?.let { ctx ->
-                    val excelFileCreator = ExcelFileCreator(ctx)
-                    val fileUri = excelFileCreator.createExcelFile(transactions, isXlsxFormat)
-                    val mimeType = if (isXlsxFormat) {
-                        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
-                    } else {
-                        "application/vnd.ms-excel"
+            if (!isSaveOperationInProgress) {
+                isSaveOperationInProgress = true
+                val dialog = DialogUtils.showLoadingDialog(requireContext())
+                lifecycleScope.launch {
+                    try {
+                        val job = withTimeoutOrNull(10000) {
+                            CoroutineScope(Dispatchers.IO).launch {
+                                val transactions = db.transactionDao().getTransactions()
+                                val isXlsxFormat = binding.switchXlsXlsx.isChecked
+                                context?.let { ctx ->
+                                    val excelFileCreator = ExcelFileCreator(ctx)
+                                    val fileUri = excelFileCreator.createExcelFile(transactions, isXlsxFormat)
+                                    val mimeType = if (isXlsxFormat) {
+                                        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+                                    } else {
+                                        "application/vnd.ms-excel"
+                                    }
+                                    val shareIntent = Intent(Intent.ACTION_SEND).apply {
+                                        type = mimeType
+                                        putExtra(Intent.EXTRA_STREAM, fileUri)
+                                        addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+                                    }
+                                    withContext(Dispatchers.Main) {
+                                        startActivity(Intent.createChooser(shareIntent, "Share Excel File"))
+                                    }
+                                }
+                            }
+                        }
+                        if (job == null) {
+                            DialogUtils.showTimeoutDialog(requireContext())
+                        }
+                    } catch (e: TimeoutCancellationException) {
+                        DialogUtils.showTimeoutDialog(requireContext())
+                    } finally {
+                        dialog.dismiss()
+                        isSaveOperationInProgress = false
                     }
-                    val shareIntent = Intent(Intent.ACTION_SEND).apply {
-                        type = mimeType
-                        putExtra(Intent.EXTRA_STREAM, fileUri)
-                        addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
-                    }
-                    startActivity(Intent.createChooser(shareIntent, "Share Excel File"))
                 }
             }
         }
 
+
         binding.buttonBelow.setOnClickListener {
-            // TODO: Handle logout button click
-            activity?.stopService(Intent(context, TokenCheckService::class.java))
-            lifecycleScope.launch {
-                withContext(Dispatchers.IO) {
-                    userViewModel.logout()
-                }
-                withContext(Dispatchers.Main) {
-                    val intent = Intent(activity, LoginActivity::class.java)
-                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-                    startActivity(intent)
+            if (!isLogoutOperationInProgress) {
+                isLogoutOperationInProgress = true
+                val dialog = DialogUtils.showLoadingDialog(requireContext())
+                lifecycleScope.launch {
+                    try {
+                        activity?.stopService(Intent(context, TokenCheckService::class.java))
+                        withContext(Dispatchers.IO) {
+                            userViewModel.logout()
+                        }
+                        withContext(Dispatchers.Main) {
+                            val intent = Intent(activity, LoginActivity::class.java).apply {
+                                flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                            }
+                            startActivity(intent)
+                        }
+                    } finally {
+                        dialog.dismiss()
+                        isLogoutOperationInProgress = false
+                    }
                 }
             }
+        }
+
+
+        binding.buttonRandomizeTransaction.setOnClickListener {
+            // TODO: RANDOMIZE TRANSACTION HERE
+            // TODO: RANDOMIZE TRANSACTION HERE
+            // TODO: RANDOMIZE TRANSACTION HERE
+
         }
     }
 
