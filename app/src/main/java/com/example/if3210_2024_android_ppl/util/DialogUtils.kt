dbd0d0e78f8400ab4c3cacbc6d60a7144041bf5d
Hidayatullah Wildan Ghaly Buchary <13521015@std.stei.itb.ac.id>
2024-04-05 10:40:12 +0700

    style: styling loading

--- app/src/main/java/com/example/if3210_2024_android_ppl/util/DialogUtils.kt
+++ app/src/main/java/com/example/if3210_2024_android_ppl/util/DialogUtils.kt
@@ -1,6 +1,8 @@
 package com.example.if3210_2024_android_ppl.util
 
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.TextView
@@ -14,6 +16,7 @@ object DialogUtils {
         builder.setView(inflater.inflate(R.layout.dialog_loading, null))
         builder.setCancelable(false)
         val dialog = builder.create()
+        dialog.window?.setBackgroundDrawableResource(R.drawable.loading_background)
         dialog.show()
         return dialog
     }
